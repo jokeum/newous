5c99fe15a65c40ae3b96a912bb7fb446647453d8
jokeum <joke0111@gmail.com>
2017-04-08 18:43:39 +0800

    Updates

---
+++ js/main.js
@@ -0,0 +1,22 @@
+(function() {
+  console.log('initialize swipers');
+
+  new Swiper('.swiper-container', {
+    direction: 'vertical',
+    keyboardControl: true,
+    mousewheelControl: true,
+    hashnav: true,
+    hashnavWatchState: true
+  });
+
+  new Swiper('.inner-swiper', {
+    direction: 'horizontal',
+    keyboardControl: true,
+    mousewheelControl: true,
+    hashnav: true,
+    hashnavWatchState: true
+  });
+
+}).call(this);
+
+ //# sourceMappingURL=main.js.map
